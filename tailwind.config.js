006731ecb2200b2b1241610dee7eacf76cc8e74e
go2023 <tetyanago2018@gmail.com>
2023-09-28 19:57:26 -0700

    Clean up

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,16 @@ module.exports = {
   // content: ["./build/**/*.{html,js}"],
   theme: {
     extend: {
+      colors: {
+        papayawhip: {
+          light: '#fef4e4',
+          DEFAULT: '#ffefd5',
+          dark: '#fee5bc',
+        }
+      },
       screens: {
         'widescreen': { 'raw': '(min-aspect-ratio: 3/2)' },
-        'tallscreen': { 'raw': '(min-aspect-ratio: 13/20)' },
+        'tallscreen': { 'raw': '(max-aspect-ratio: 13/20)' },
       },
       keyframes: {
         'open-menu': {
